f4e5d12fea58ab8b1097e00e78020b64d8d8c60f
Ha Thi <ha.pham@sapotacorp.vn>
2024-06-19 15:21:53 +0700

    Dark/ light mode and register

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+import RegisterForm from "@/app/(auth)/register/register-form";
+
+export default function RegisterPage() {
+  
+  return (
+    <div>
+      <h1>Đăng kí</h1>
+      <RegisterForm />
+    </div>
+  )
+}
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
 
-const inter = Inter({ subsets: ["latin"] });
+
+const inter = Inter({ subsets: ["vietnamese"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +18,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>        
+      <body className={inter.className}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            {children}
+          </ThemeProvider>
+      </body>
+       
+
     </html>
   );
 }
